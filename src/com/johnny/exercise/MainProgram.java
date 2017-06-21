791a58fe8135eed886f0d4a206736823bac836df
johnn <johnn@LAPTOP-CSG390HQ>
2017-06-21 08:47:51 +0100

    commit

--- src/com/johnny/exercise/MainProgram.java
+++ src/com/johnny/exercise/MainProgram.java
@@ -5,33 +5,47 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import org.apache.commons.io.FileUtils;
-
 import com.johnny.exercise.messages.Message;
 import com.johnny.exercise.messages.MessageTypeOne;
+import com.johnny.exercise.messages.MessageTypeThree;
+import com.johnny.exercise.messages.MessageTypeTwo;
 import com.johnny.exercise.sales.Sale;
-import com.sun.javafx.font.PGFont;
 
 public class MainProgram {
 
-	private boolean running;
+	private boolean running = true;
 	private ArrayList<Message> messageList = null;
 	private static String INPUTFILEFOLDER = "C:/Users/johnn/git/ProgrammingExercise/InputFiles";
+	private static String OUTPUTFILEFOLDER = "C:/Users/johnn/git/ProgrammingExercise/OutputFiles";
+	private static int messageCount = 0;
+	private static final int SALEREPORTTRIGGER = 10;
+	private static final int ADJUSTMENTREPORTTRIGGER = 50;
+	private static final boolean isDebugModeOn = true;
 
-	public void readAndParse(){
-		//System.out.println();
+	public MainProgram(){
+		this.messageList = new ArrayList<Message>();
+	}
+
+	public static void main(String[] args) {
+
+		MainProgram pg = new MainProgram();
+
+		//process files
 		File folder = new File(INPUTFILEFOLDER);
-
 		File[] listOfFiles = folder.listFiles();
-
 		for (int i = 0; i < listOfFiles.length; i++) {
 			File file = listOfFiles[i];
 			if (file.isFile() && file.getName().endsWith(".csv")) {
+				List<String> lines = null;
 				try {
-					String content = FileUtils.readFileToString(file);
-					//System.out.println(content);
-					List<String> messageValueList = Arrays.asList(content.split(","));
+					lines = FileUtils.readLines(file);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+
+				for (String line : lines) {
+					List<String> messageValueList = Arrays.asList(line.split(","));
 
 					int messageType = 0;
 
@@ -41,45 +55,56 @@ public class MainProgram {
 
 					switch (messageType){
 					case 1:
-						Message tmpMsg = new MessageTypeOne();
-						Sale tmpsl = new Sale(messageValueList.get(1),Float.parseFloat(messageValueList.get(2)));
-						tmpMsg.setSaleDetails(tmpsl);
-						this.messageList.add(tmpMsg);
+						Message tmpMsg1 = new MessageTypeOne();
+						Sale tmpsl1 = new Sale(messageValueList.get(1),Float.parseFloat(messageValueList.get(2)));
+						tmpMsg1.setSaleDetails(tmpsl1);
+						pg.messageList.add(tmpMsg1);
 						break;
 					case 2:
-						
-						
-						
+						Message tmpMsg2 = new MessageTypeTwo();
+						Sale tmpsl2 = new Sale(messageValueList.get(1),Float.parseFloat(messageValueList.get(2)));
+						tmpMsg2.setSaleDetails(tmpsl2);
+						pg.messageList.add(tmpMsg2);
+						break;
+					case 3:
+						Message tmpMsg3 = new MessageTypeThree();
+
+						Sale tmpsl3 = null;
+						if (messageValueList.get(2) != null && messageValueList.get(2).length() != 0){
+							tmpsl3 = new Sale(messageValueList.get(1),Float.parseFloat(messageValueList.get(2)));
+						}
+						else {
+							tmpsl3 = new Sale(messageValueList.get(1),-1);
+						}
+						tmpMsg3.setSaleDetails(tmpsl3);
+						pg.messageList.add(tmpMsg3);
+						break;
 					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				} finally {
-					this.cleanup(file);
+					messageCount += 1;
+
+					if (messageCount % SALEREPORTTRIGGER == 0){
+
+					}
+
+					if (messageCount % ADJUSTMENTREPORTTRIGGER == 0){
+
+					}
 				}
 			}
+			//add move file here
+			if (isDebugModeOn != true){
+				File destinationFile = new File(OUTPUTFILEFOLDER + "/" + file.getName());
+				try {
+					FileUtils.moveFile(file, destinationFile);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
 		}
-	}
-
-	public void process(){
-		System.out.println();
-
-	}
-
-	public void cleanup(File tmp){
-		System.out.println();
-	}
-
-	public MainProgram(){
-		this.running = true;
-		this.messageList = new ArrayList<Message>();
-	}
-
-	public static void main(String[] args) {
-
-		MainProgram pg = new MainProgram();
-
-		pg.readAndParse();
-
+		//System.out.println(messageList);
+		//pg.readAndParse();
+		//pg.process();
 	}
 
 }
--- src/com/johnny/exercise/messages/Message.java
+++ src/com/johnny/exercise/messages/Message.java
@@ -5,6 +5,7 @@ import com.johnny.exercise.sales.Sale;
 public abstract class Message {
 	
 	private Sale saleDetails;
+	private boolean isProcessed = false;
 
 	public Sale getSaleDetails() {
 		return saleDetails;
@@ -12,6 +13,14 @@ public abstract class Message {
 
 	public void setSaleDetails(Sale saleDetails) {
 		this.saleDetails = saleDetails;
+	}
+
+	public boolean isProcessed() {
+		return isProcessed;
+	}
+
+	public void setProcessed(boolean isProcessed) {
+		this.isProcessed = isProcessed;
 	}
 	
 }
